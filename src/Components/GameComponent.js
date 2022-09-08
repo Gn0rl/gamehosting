006f5a42be6bc45933137c22fdb66acb6b3f0aee
QWERN-9876r <105445546+QWERN-9876r@users.noreply.github.com>
2022-09-08 21:28:55 +0400

    Add files via upload

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -6,7 +6,16 @@ const GameComponent = (props) => {
                 <img src={terrariaImg} alt={'a'} className={'GameImg'}/>
                 <span><h1 className={'gameName'}>{props.GameName}</h1></span>
                 <p className="description">{props.description}</p>
+                <span className={'info'}>
+                    <h1 className={'gameVersion'}>v. {props.version}</h1>
+                    <h2 className={'gameVersion plathorm'}>{props.plathorm}</h2>
+                </span>
                 <button className="BtnDonload"><a href='https://disk.yandex.ru/d/EOHk86vIT9XVEQ'>Скачать</a></button>
+            </div>
+            <div className="images">
+                <img src={props.Images[0]} className="img"/>
+                <img src={props.Images[1]} className="img"/>
+                <img src={props.Images[2]} className="img"/>
             </div>
         </div>
     );
