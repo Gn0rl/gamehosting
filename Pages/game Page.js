48b1e653c0f3df84b972f8b11ed0f4bd2051aa75
QWERN-9876r <105445546+QWERN-9876r@users.noreply.github.com>
2022-09-08 21:46:21 +0400

    Add files via upload

--- Pages/game Page.js
+++ Pages/game Page.js
@@ -1,11 +1,17 @@
 import GameComponent from "../Components/GameComponent";
-import Top from "../Components/Top.js"
+import GameImg1 from '../Images/imageInGame1.jpeg';
+import GameImg2 from '../Images/imageInGame2.jpeg';
+import GameImg3 from '../Images/imageInGame3.jpeg';
+import Top from '../Components/Top';
+
 
 const GamePage = () => {
     return (
-        <div className="container">
+        <div>
             <Top/>
-            <GameComponent GameName={'Terraria'} description={'Описание игры terraria'}/>
+            <div className="container">
+                <GameComponent GameName={'Terraria'} description={'Описание игры terraria'} version={'1.4'} plathorm={'PC, Android'} Images={[GameImg1, GameImg2, GameImg3]}/>
+            </div>
         </div>
     );
 };
