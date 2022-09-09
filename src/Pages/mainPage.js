84ae6f1f61bb05f11c41213d11d5576024714993
QWERN-9876r <105445546+QWERN-9876r@users.noreply.github.com>
2022-09-09 17:24:00 +0400

    Add files via upload

--- src/Pages/mainPage.js
+++ src/Pages/mainPage.js
@@ -1,10 +1,14 @@
-
+import MainComponent from "../Components/MainComponent";
 import Top from '../Components/Top';
 
 
 const GamePage = () => {
     return (
         <div>
+            <Top/>
+            <div className={"Container"}>
+                <MainComponent/>
+            </div>
         </div>
     );
 };
