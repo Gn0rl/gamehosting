b07d17a0406c4b6a458d064ab21846b9ff3a8536
QWERN-9876r <105445546+QWERN-9876r@users.noreply.github.com>
2022-09-10 15:26:35 +0400

    Add files via upload

--- src/Pages/CreatePanel.js
+++ src/Pages/CreatePanel.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Top from "../Components/Top";
+import CreatePanelComponent from "../Components/CreatePanelComponent";
+import "../css/CreatePanelComponent.css";
 
 const CreatePanel = () => {
     return (
         <div>
             <Top/>
+            <CreatePanelComponent/>
         </div>
     );
 };
