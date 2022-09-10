9e5c99eb7aa9b363317c819e11631127b396eec7
QWERN-9876r <105445546+QWERN-9876r@users.noreply.github.com>
2022-09-10 08:19:54 +0400

    Add files via upload

---
+++ src/Pages/CreatePanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Top from "../Components/Top";
+
+const CreatePanel = () => {
+    return (
+        <div>
+            <Top/>
+        </div>
+    );
+};
+
+export default CreatePanel;
