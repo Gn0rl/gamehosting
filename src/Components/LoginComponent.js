0d4c699c69cc6efb91575d962668313c92a56cc8
QWERN-9876r <dimadobriy111@gmail.com>
2022-09-11 19:35:17 +0400

    create input name in login

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -3,15 +3,18 @@ import "../css/login.css"
 
 const LoginComponent = () => {
     let [email, setEmail] = useState(''),
-        [password, setPassword] = useState('');
+        [password, setPassword] = useState(''),
+        [userName, setUserName] = useState('');
     return (
         <div style={{textAlign: "center"}}>
             <div className="loginBlock">
                 <h1>Войти</h1>
                 <input placeholder="email" className="input" value={email}
                        onChange={(el) => {setEmail(el.target.value)}}/>
-                <input placeholder="password" className="input" type="password" value={password}
+                <input placeholder="password" className="input" type="email" value={password}
                        onChange={(el) => {setPassword(el.target.value)}}/>
+                <input placeholder="name" className="input" type="text" value={userName}
+                       onChange={(el) => {setUserName(el.target.value)}}/>
                 <div className="buttons">
                     <button>Войти</button>
                     <button>Зарегистрироватся</button>
